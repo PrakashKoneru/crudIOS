2e3109f008694e77ee07c9ba148eb55cbba395d0
Prakash K <sivaprakashkoneru67@gmail.com>
2025-03-16 11:51:30 -0400

    Made the IOS app

---
+++ TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Siva prakash Koneru on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoApp/TodoAppTests/TodoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTests.swift
+//  TodoAppTests
+//
+//  Created by Siva prakash Koneru on 3/16/25.
+//
+
+import Testing
+@testable import TodoApp
+
+struct TodoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
